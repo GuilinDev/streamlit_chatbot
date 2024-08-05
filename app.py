e2679a4814a1b8542cdaa0f9065b8954501128b9
Guilin Zhang <GuilinDev@gmail.com>
2024-08-04 22:14:42 -0400

    Use gemini pro

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
+import os
 import streamlit as st
-from pydantic import BaseModel
-from langchain_community.chat_models.ollama import ChatOllama
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.output_parsers import StrOutputParser
 from langchain.schema import AIMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_google_genai import ChatGoogleGenerativeAI
+from pydantic import BaseModel
 
-model = ChatOllama(model_name='llama2', streaming=True)
+os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
+
+llm = ChatGoogleGenerativeAI(model="gemini-pro", streaming=True)
+
 memory_key = 'history'
-
 prompt = ChatPromptTemplate.from_messages([
     MessagesPlaceholder(variable_name=memory_key),
     ('human', '{input}')
 ])
 
-
 class Message(BaseModel):
     content: str
     role: str
 
-
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
 
 def to_message_placeholder(messages):
     return [
@@ -29,11 +29,10 @@ def to_message_placeholder(messages):
         for message in messages
     ]
 
-
 chain = {
-            'input': lambda x: x['input'],
-            'history': lambda x: to_message_placeholder(x['messages'])
-        } | prompt | model | StrOutputParser()
+    'input': lambda x: x['input'],
+    'history': lambda x: to_message_placeholder(x['messages'])
+} | prompt | llm | StrOutputParser()
 
 # Use st.columns() with a list of relative widths
 left, right = st.columns([0.7, 0.3])
@@ -45,15 +44,15 @@ with left:
             st.write(message['content'])
 
     # receive user's input, save into session_state
-    prompt = st.chat_input("Hello, what can I do for you?")
-    if prompt:
-        st.session_state.messages.append(Message(content=prompt, role="human").model_dump())
+    user_input = st.chat_input("Hello, what can I do for you?")
+    if user_input:
+        st.session_state.messages.append(Message(content=user_input, role="human").model_dump())
         with st.chat_message("human"):
-            st.write(prompt)
+            st.write(user_input)
 
         # get LLM response and display
         with st.chat_message("ai"):
-            response = st.write_stream(chain.stream({'input': prompt, 'messages': st.session_state.messages}))
+            response = st.write_stream(chain.stream({'input': user_input, 'messages': st.session_state.messages}))
 
         st.session_state.messages.append(Message(content=response, role='ai').model_dump())
 
